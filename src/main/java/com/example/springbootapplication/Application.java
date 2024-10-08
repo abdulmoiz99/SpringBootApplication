15807dc8221bcdee0b54f4a6f56f5d17613dd3e2
Abdul Moiz <moiza0799@gmail.com>
2024-10-08 13:32:11 -0500

    One to one bi directional

--- src/main/java/com/example/springbootapplication/Application.java
+++ src/main/java/com/example/springbootapplication/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.example.springbootapplication.model.association.onetooneUNI")
+@EntityScan(basePackages = "com.example.springbootapplication.model.association.onetooneBI")
 public class Application {
 
     public static void main(String[] args) {
